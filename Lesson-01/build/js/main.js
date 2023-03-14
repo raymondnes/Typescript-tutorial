e6fdc0c07077ed1f050c47752d9780c88cb67877
raymondnes <raynelreign@gmali.com>
2023-03-14 15:58:24 +0400

    defined new variables

--- Lesson-01/build/js/main.js
+++ Lesson-01/build/js/main.js
@@ -1,3 +1,6 @@
 "use strict";
 let username = 'Noela';
 console.log(username);
+let a = 12;
+let b = '6';
+let c = 2;
--- Lesson-01/src/main.ts
+++ Lesson-01/src/main.ts
@@ -1,2 +1,6 @@
 let username = 'Noela'
 console.log(username)
+
+let a = 12
+let b = '6'
+let c = 2
