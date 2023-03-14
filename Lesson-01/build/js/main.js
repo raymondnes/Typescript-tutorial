59abb059dcd3f33ea4694d47ccb7a3b27de3ebc1
raymondnes <raynelreign@gmali.com>
2023-03-14 20:20:56 +0400

    noEmitOnError

--- Lesson-01/build/js/main.js
+++ Lesson-01/build/js/main.js
@@ -2,7 +2,7 @@
 let username = 'Noela';
 console.log(username);
 let a = 12;
-let b = '6';
+let b = 6;
 let c = 2;
 console.log(a / b);
 console.log(c * b);
--- Lesson-01/src/main.ts
+++ Lesson-01/src/main.ts
@@ -1,9 +1,9 @@
 let username = 'Noela'
 console.log(username)
 
-let a = 12
-let b = '6'
-let c = 2
+let a: number = 12
+let b: number = 6
+let c: number = 2
 
 console.log(a / b)
 
